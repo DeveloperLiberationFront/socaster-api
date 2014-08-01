14cd6bb1ae88be5a7c74ded8a195f46c0cb73d92
Samuel Christie <emailium@gmail.com>
2014-08-01 12:00:14 -0400

    Set logging options for tornado

--- run.py
+++ run.py
@@ -12,8 +12,9 @@ from tornado.options import options
 from validator import Validator
 from auth import SocasterAuth
 
-options.logging = 'debug'
-options.log_to_stderr = True
+options['logging'].set('debug')
+options['log_to_stderr'].set(True)
+options.parse_command_line()
 
 app = Eve(auth=SocasterAuth, validator=Validator)
 
